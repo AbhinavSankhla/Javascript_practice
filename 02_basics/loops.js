67ee95e29d0d90d3c865abdbf7d00ffac8e52b70
unknown <abhinavsankhla16@gmail.com>
2025-01-16 09:02:13 +0530

    loops

---
+++ 02_basics/loops.js
@@ -0,0 +1,36 @@
+//01. for loop - The for loop is used when you know how many times you want to iterate beforehand.
+
+// for (let i=0; i<5; i++){
+//     console.log(i)
+// }
+
+
+// 02. while loop - The while loop is used when you want to repeat a block of code as long as a condition is true.
+
+// let i = 0;
+// while (i<3){
+//     console.log(i);
+//     i++
+// }
+
+
+// 03. do while loop - The do...while loop is similar to the while loop, but it executes the code block at least once, even if the condition is initially false. syntex-
+// do {
+//     // Code to execute
+// } while (condition);
+
+// let i = 4;
+// do{
+//     console.log(i);
+//     i++
+// } while(i<3)
+
+//04. forEach loop - The forEach loop is a higher-order function specifically for iterating over arrays. It applies a provided callback function to each element of the array.
+// Syntax- array.forEach((element, index, array) => {
+//     // Code to execute
+// });
+
+// const arr = [10,20,30,40,50]
+// arr.forEach((value,index)=>{
+//     console.log(value,index)
+// })
---
+++ 02_basics/star_pattern.js
@@ -0,0 +1,25 @@
+// 01. 
+// (a).repeat() method, which repeats a string a specified number of times
+// for (let i=1; i<=5; i++){
+//     console.log("*".repeat(i));
+// }
+
+// (b) Using String Concatenation
+let str = ""
+for (let i=1; i<=5; i++){
+    str = str + "*"         //str += "*"
+    console.log(str)
+}
+
+
+// (c) Using Nested Loops
+// for (let i = 1; i <= 5; i++) {
+//     let str = "";
+//     for (let j = 1; j <= i; j++) {
+//         str += "*";
+//     }
+//     console.log(str);
+// }
+
+
+
